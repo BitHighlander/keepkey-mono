7ddd54652ada3498924e844a15d9a2314bdaf94e
highlander <bithighlander@gmail.com>
2021-10-18 13:28:54 -0600

    bridge work

--- modules/keepkey-brige/src/index.ts
+++ modules/keepkey-brige/src/index.ts
@@ -12,10 +12,18 @@ const TAG = " | "+ pjson.name +" | "
 //hardware
 let Hardware = require("@keepkey/keepkey-hardware")
 
+let KEEPKEY_ADAPTER:any = {}
+
 const log = require('@pioneer-platform/loggerdog')()
 const express = require( "express" );
+const bodyParser = require("body-parser");
+const cors = require('cors')
 const app = express();
-let API_PORT:any = process.env["API_PORT_BRIDGE"] || "3000"
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let API_PORT:any = process.env["API_PORT_BRIDGE"] || "1646"
 API_PORT = parseInt(API_PORT)
 
 // define a route handler for the default home page
@@ -33,6 +41,24 @@ app.get( "/locked", ( req:any, res:any ) => {
     let output = Hardware.isLocked()
     res.send( output );
 } );
+
+//bridge
+app.post('/exchange/device', function (req:any, res:any) {
+    let body = req.body
+    log.info("body: ",body)
+    log.info("KEEPKEY_ADAPTER: ",KEEPKEY_ADAPTER)
+    let msg = Buffer.from(req.body, "hex")
+    KEEPKEY_ADAPTER.transport.writeChunk(msg)
+    let output = "foobar"
+    res.send( output );
+});
+
+//catchall
+app.use((err:any, req:any, res:any, next:any) => {
+    const { status = 500, message = 'something went wrong. ', data = {} } = err
+    log.error(message)
+    res.status(status).json({ message, data })
+})
 
 const start_server = function(){
     try{
@@ -52,8 +78,9 @@ module.exports = {
     allDevices: function () {
         return Hardware.allDevices();
     },
-    startKeepkey: function () {
-        return Hardware.start();
+    startKeepkey: async function () {
+        KEEPKEY_ADAPTER = await Hardware.start()
+        return KEEPKEY_ADAPTER;
     },
     hardwareState: function () {
         return Hardware.state();
