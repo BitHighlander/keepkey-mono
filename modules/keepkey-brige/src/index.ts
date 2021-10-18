f1187cdbf317770ab869703225077f445a7a92d8
highlander <bithighlander@gmail.com>
2021-10-18 15:18:33 -0600

    simplify bridge

--- modules/keepkey-brige/src/index.ts
+++ modules/keepkey-brige/src/index.ts
@@ -1,19 +1,9 @@
-require('dotenv').config()
-require('dotenv').config({path:"./.env"})
-require('dotenv').config({path:"./../.env"})
-require('dotenv').config({path:"./../../.env"})
-require('dotenv').config({path:"../../../.env"})
-require('dotenv').config({path:"../../../../.env"})
-require('dotenv').config({path:"./../../../../.env"})
-
-const pjson = require('../package.json');
-const TAG = " | "+ pjson.name +" | "
-
-//hardware
-let Hardware = require("@keepkey/keepkey-hardware")
-
-let KEEPKEY_ADAPTER:any = {}
-
+/*
+    KeepKey Bridge module
+ */
+import * as core from "@shapeshiftoss/hdwallet-core"
+import { NodeWebUSBKeepKeyAdapter, NodeWebUSBAdapterDelegate } from "@shapeshiftoss/hdwallet-keepkey-nodewebusb";
+const adapter = NodeWebUSBKeepKeyAdapter.useKeyring(new core.Keyring())
 const log = require('@pioneer-platform/loggerdog')()
 const express = require( "express" );
 const bodyParser = require("body-parser");
@@ -26,31 +16,30 @@ app.use(bodyParser.json());
 let API_PORT:any = process.env["API_PORT_BRIDGE"] || "1646"
 API_PORT = parseInt(API_PORT)
 
-// define a route handler for the default home page
-app.get( "/info", ( req:any, res:any ) => {
-    let output = Hardware.info()
-    res.send( output );
-} );
-
-app.get( "/state", ( req:any, res:any ) => {
-    let output = Hardware.state()
-    res.send( output );
-} );
-
-app.get( "/locked", ( req:any, res:any ) => {
-    let output = Hardware.isLocked()
-    res.send( output );
-} );
+let WALLET_TRANSPORT:any = {}
 
 //bridge
-app.post('/exchange/device', function (req:any, res:any) {
-    let body = req.body
-    log.info("body: ",body)
-    log.info("KEEPKEY_ADAPTER: ",KEEPKEY_ADAPTER)
-    let msg = Buffer.from(req.body, "hex")
-    KEEPKEY_ADAPTER.transport.writeChunk(msg)
-    let output = "foobar"
-    res.send( output );
+app.all('/exchange/device', async function (req:any, res:any, next:any) {
+    try{
+        if(req.method === 'GET'){
+            let resp = await WALLET_TRANSPORT.readChunk()
+            let output = {
+                data:Buffer.from(resp).toString('hex')
+            }
+            res.status(200).json(output)
+        } else if(req.method === 'POST') {
+            let body = req.body
+            let msg = Buffer.from(body.data, "hex")
+            WALLET_TRANSPORT.writeChunk(msg)
+            res.status(200).json({ })
+        } else {
+            throw Error('unhandled')
+        }
+        next()
+    }catch(e){
+        log.error(e)
+        throw e
+    }
 });
 
 //catchall
@@ -60,9 +49,8 @@ app.use((err:any, req:any, res:any, next:any) => {
     res.status(status).json({ message, data })
 })
 
-const start_server = function(){
+const start_server = async function(){
     try{
-        // start the Express server
         app.listen( API_PORT, () => {
             console.log( `server started at http://localhost:${ API_PORT }` );
         } );
@@ -72,35 +60,13 @@ const start_server = function(){
 }
 
 module.exports = {
-    startServer: function () {
+    startServer: async function () {
         return start_server();
     },
-    allDevices: function () {
-        return Hardware.allDevices();
-    },
     startKeepkey: async function () {
-        KEEPKEY_ADAPTER = await Hardware.start()
-        return KEEPKEY_ADAPTER;
-    },
-    hardwareState: function () {
-        return Hardware.state();
-    },
-    hardwareLocked: function () {
-        return Hardware.isLocked();
-    },
-    hardwareInfo: function () {
-        return Hardware.info();
-    },
-    hardwareWipe: function () {
-        return Hardware.wipe();
-    },
-    hardwareLoad: function (mnemonic:string) {
-        return Hardware.load(mnemonic);
-    },
-    hardwareShowPin: function () {
-        return Hardware.displayPin();
-    },
-    hardwareEnterPin: function (pin:string) {
-        return Hardware.enterPin(pin);
+        let device = await adapter.getDevice()
+        WALLET_TRANSPORT = await adapter.getTransportDelegate(device)
+        await WALLET_TRANSPORT.connect?.()
+        return device;
     }
 }
